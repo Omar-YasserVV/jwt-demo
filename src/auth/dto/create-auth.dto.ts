3375194dd83523d8a7b210e18cd91d163d84b5bb
Omar-YasserVV <bakryasser744@gmail.com>
2025-11-25 06:18:08 +0200

    edit dto and add validator

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,11 @@
-export class CreateAuthDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAuthDto {
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
